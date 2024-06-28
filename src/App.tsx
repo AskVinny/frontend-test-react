7d15a25b9243f641d46c5aeabe23c48dffa587fa
Rory buchanan <rorybuchanan@Rorys-MacBook-Air.local>
2024-06-28 08:31:07 +0100

    updating url

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./styles.css";
 // 2. Add an input field to accept an email.
 // 3. Add a button that passes the input value to the parent component.
 // 4. In the parent component, add logic to send the value to a backend with a POST request to
-//the following url https://webhook.site/6064735c-c7f2-4584-ba9d-1f0e80f32721. Along with the email, send your github username in the JSON.
+//the following url https://webhook.site/30b1bdd1-b233-4262-b3f0-918cb9d94e71. Along with the email, send your github username in the JSON.
 // 5. Add styling to the button (Button) and input (Input) using the ShadCN Component library here: https://ui.shadcn.com/docs/components/input
 // 6. Please return your video, along with your cloned github repo link.
 
